fced21d2b8dc25a56bdfc6ee2f9066a74ec85625
Rashed Obaid <me@rashedobaid.com>
2026-02-28 22:23:59 +0800

    Set tmux on Ghostty

--- modules/dotfiles/ghostty/default.nix
+++ modules/dotfiles/ghostty/default.nix
@@ -7,6 +7,7 @@
       theme = "Nord";
       font-size = 12;
       font-family = "JetBrainsMono Nerd Font Mono";
+      command = "/run/current-system/sw/bin/tmux";
       adjust-cell-height = "25%";
       font-thicken = true;
       shell-integration-features = "ssh-terminfo";
